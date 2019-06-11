e6d5929f4e955c33c878229860357d7f77d451a9
harry <912043310@qq.com>
2019-06-11 22:48:27 +0800

    Add load balance component - ribbon

--- eureka-client/src/main/java/cn/harry/cloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/harry/cloud/eurekaclient/EurekaClientApplication.java
@@ -3,11 +3,19 @@ package cn.harry.cloud.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
+    @LoadBalanced
+    @Bean(value = "restTemplate")
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
--- eureka-client/src/main/java/cn/harry/cloud/eurekaclient/controller/HelloController.java
+++ eureka-client/src/main/java/cn/harry/cloud/eurekaclient/controller/HelloController.java
@@ -1,15 +1,24 @@
 package cn.harry.cloud.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+    @Autowired
+    @Qualifier("restTemplate")
+    private RestTemplate restTemplate;
+
     @GetMapping
     public String hello(){
-        return "hello, this is eureka client for consumer";
+        String responseMsg = this.restTemplate.getForObject("http://eureka-client-provider/hello",String.class);
+        System.out.println(responseMsg);
+        return "responseMsg: "+responseMsg;
     }
 }
